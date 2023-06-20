1732cfe6e0e3c9797b0775e33a68181cccdc23f5
Priyanka <priyanka_terala@epam.com>
2023-06-20 16:49:25 +0530

    decompress a compressed file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs';
+import {fileURLToPath} from 'url';
+import { pipeline } from 'stream';
+import zlib from 'zlib';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const source = createReadStream(path.join(__dirname, 'files', 'archive.gz'));
+	const target = createWriteStream(path.join(__dirname, 'files','fileToCompress.txt'));
+
+	pipeline(source, zlib.createGunzip(), target, (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  },);
 };
 
 await decompress();
