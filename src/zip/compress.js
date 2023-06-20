37503516845a06c2981d0732bb7e09e8e0de69c2
Priyanka <priyanka_terala@epam.com>
2023-06-20 16:45:30 +0530

    compress a file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs';
+import {fileURLToPath} from 'url';
+import { pipeline } from 'stream';
+import zlib from 'zlib';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+	const source = createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+	const target = createWriteStream(path.join(__dirname, 'files','archive.gz'));
+
+	pipeline(source, zlib.createGzip(), target, (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  },);
 };
 
 await compress();
