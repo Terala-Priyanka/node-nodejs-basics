9d75ec05e860e4abc4adad4b7dbe5c115dd5b422
Priyanka <priyanka_terala@epam.com>
2023-06-20 18:38:37 +0530

    spwan child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { fork } from 'child_process';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+// import { stdin, stdout } from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	const srcFile = join(__dirname, 'files', 'script.js');
+	const childProcess = fork(srcFile, args, { silent: true });
+
+	process.stdin.pipe(childProcess.stdin);
+	childProcess.stdout.pipe(process.stdout);
+
+	childProcess.stdout.on('data', (msg) => console.log(`Message from child: ${msg}`));
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Hi', 'Hello', 'Hola']);
